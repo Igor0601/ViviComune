a9c0372d6cd3bda9f66ec04e5155e90f4032a03b
Igor0601 <igor.casadidio@studenti.unicam.it>
2024-05-23 11:44:25 +0200

    test commit

--- src/main/java/it/cs/unicam/ViviComune/ViviComuneApplication.java
+++ src/main/java/it/cs/unicam/ViviComune/ViviComuneApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ViviComuneApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ViviComuneApplication.class, args);
+		System.out.println("Commit");
 	}
 
 }
