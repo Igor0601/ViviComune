b8886bea41686ea318874844e18dba4767506ae6
Federico <federico.sbarbati@studenti.unicam.it>
2024-05-23 11:55:26 +0200

    si

--- src/main/java/it/cs/unicam/ViviComune/ViviComuneApplication.java
+++ src/main/java/it/cs/unicam/ViviComune/ViviComuneApplication.java
@@ -9,7 +9,7 @@ public class ViviComuneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ViviComuneApplication.class, args);
-		System.out.println("Commit");
+		System.out.println("Commit si");
 	}
 
 }
